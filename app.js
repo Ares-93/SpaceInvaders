c13a1003811e66cc4fd44698a80fe51d618d2d5e
Mohammad Haider <arzhdr@gmail.com>
2024-05-30 13:12:35 -0400

    fixed issue in linking the style sheet (i can spell i promise)

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const resultDisplay = document.querySelector(".results");
 
 const width = 15;
 
-for (let i = 0; i < width * width; i++) {
+for (
+  let i = 0;
+  i < width * width;
+  i++ //loops 225 times (15*15=225)
+) {
   const square = document.createElement("div");
   grid.appendChild(square);
 }
